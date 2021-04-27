01d00678f3ba779be7723bc22a4d012d7d06f7ba
Ryan Hawkins <>
2021-04-27 22:03:54 +0000

    Added logic

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 
+var flip_button = document.getElementById("flipButton")
+
+flip_button.addEventListener("click", flipCoin)
+
+
+// Flip coin
+function flipCoin() {
+    return Math.round(Math.random())
+}
+
+// Show the result of the coin flip
+function showResult() {
+    document.querySelector("p").innerText = flipCoin()
+}
+
+
+console.log("hello")
