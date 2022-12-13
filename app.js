21691d87ffafc07354d43b495be26e95bb19c538
Ryan Hawkins <rkhawkins80@hotmail.com>
2022-12-13 21:50:44 +0000

    Update:  functioning

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const flip_button = document.getElementById("flipButton")
 flip_button.addEventListener("click", flipCoin)
 
 function flipCoin() {
-    return Math.random() < 0.5 ? "Heads" : "Tails"
-    showResult()
+    
+    const result = Math.random() < 0.5 ? "Heads" : "Tails"
+    showResult(result)
 }
 
 // Show the result of the coin flip
-function showResult() {
-    document.querySelector("p").innerText = flipCoin()
+function showResult(result) {
+    document.querySelector("p").innerText = result
 }
