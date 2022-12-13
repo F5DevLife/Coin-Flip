6d54d9e36a04451dfbc36dddb3e3656ac05115bd
Ryan Hawkins <rkhawkins80@hotmail.com>
2022-12-13 15:46:56 -0600

    Update:  Fixed logic / corrected syntax

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-
-var flip_button = document.getElementById("flipButton")
+const flip_button = document.getElementById("flipButton")
 
 flip_button.addEventListener("click", flipCoin)
 
-
-// Flip coin
 function flipCoin() {
-    return Math.round(Math.random())
+    return Math.random() < 0.5 ? "Heads" : "Tails"
+    showResult()
 }
 
 // Show the result of the coin flip
 function showResult() {
     document.querySelector("p").innerText = flipCoin()
 }
-
-
-console.log("hello")
